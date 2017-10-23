dbd5b4de5d2317cd82bd39f99f2acab71aab4215
sl-prv <sven@laengler.eu>
2017-10-23 16:53:29 +0200

    Fix: collision detection

--- src/com/thegame/game/mob/Mob.java
+++ src/com/thegame/game/mob/Mob.java
@@ -69,7 +69,7 @@ public abstract class Mob extends Entity {
 		boolean solid = false;		
 		for (int c = 0; c < 4; c++) {
 			double xt = ((x + xa) - c % 2 * 15) / 16;
-			double yt = ((y + ya) - c % 2 * 15) / 16;
+			double yt = ((y + ya) - c % 2 * 13) / 14;
 			int ix = (int) Math.ceil(xt);
 			int iy = (int) Math.ceil(yt);
 			if (c % 2 == 0) ix = (int) Math.floor(xt);
