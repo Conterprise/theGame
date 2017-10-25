86775baa90dc4f414ef6763bdb08ec90c611f965
sl-prv <sven@laengler.eu>
2017-10-25 23:43:23 +0200

    merge local changes into branch

--- src/com/thegame/game/tile/VoidTile.java
+++ src/com/thegame/game/tile/VoidTile.java
@@ -8,7 +8,7 @@ public class VoidTile extends Tile {
 	public VoidTile(Sprite sprite) {
 		super(sprite);
 	}
-	
+		
 	public void render(int x, int y, Screen screen) {	
 		screen.renderTile(x << 4, y << 4, this);
 	}
